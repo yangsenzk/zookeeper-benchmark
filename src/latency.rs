1f8c002c0bdf15dc6fb4a40668f5d832c25ee4b2
yangzhengkun <yangzhengkun@bytedance.com>
2023-03-27 17:41:21 +0800

    first commit

---
+++ src/latency.rs
@@ -0,0 +1,120 @@
+use std::collections::HashMap;
+
+use serde::Serialize;
+
+#[derive(Serialize, Clone, Debug)]
+pub struct RequestLatency {
+    op_latency: HashMap<String, OpResult>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+struct OpResult {
+    op_cnt: u32,
+    bucket: HashMap<u32, u32>,
+    // #[serde(skip_serializing)]
+    detail_quantile: Vec<(u32, f32)>,
+    short_quantile: HashMap<String, u32>,
+    qps: f32,
+}
+
+impl OpResult {
+    pub fn new() -> OpResult {
+        OpResult {
+            op_cnt: 0,
+            bucket: HashMap::new(),
+            detail_quantile: vec![],
+            short_quantile: HashMap::new(),
+            qps: 0.0,
+        }
+    }
+}
+
+impl RequestLatency {
+    pub fn new() -> RequestLatency {
+        RequestLatency {
+            op_latency: HashMap::new()
+        }
+    }
+
+    pub fn insert(&mut self, op: String, val: u32) {
+        if !self.op_latency.contains_key(op.as_str()) {
+            self.op_latency.insert(op.to_string(), OpResult::new());
+        }
+
+        match self.op_latency.get_mut(op.as_str()) {
+            Some(op_result) => {
+                match op_result.bucket.get(&val) {
+                    Some(cur_cnt) => {
+                        op_result.bucket.insert(val, cur_cnt + 1);
+                    }
+                    None => {
+                        op_result.bucket.insert(val, 1);
+                    }
+                }
+                op_result.op_cnt += 1;
+            }
+            None => {
+                self.op_latency.insert(op.to_string(), OpResult::new());
+            }
+        }
+    }
+
+    fn update_quantile(&mut self) {
+        for (_op, result) in self.op_latency.iter_mut() {
+            let mut keys: Vec<&u32> = result.bucket.keys().collect();
+            keys.sort();
+            let mut cumulative_sum = 0;
+            result.detail_quantile = Vec::new();
+            for key in keys {
+                let cnt = result.bucket.get(key).unwrap_or(&0u32);
+                cumulative_sum += cnt;
+
+                let percent = cumulative_sum as f32 / result.op_cnt as f32;
+                result.detail_quantile.push((*key, percent));
+
+                let k;
+                if percent <= 0.90 {
+                    k = String::from("0.90");
+                } else if percent <= 0.95 {
+                    k = String::from("0.95");
+                } else if percent <= 0.99 {
+                    k = String::from("0.99");
+                } else if percent <= 0.999 {
+                    k = String::from("0.999");
+                } else {
+                    k = String::from("1.00");
+                }
+
+                let p = result.short_quantile.get(k.as_str()).unwrap_or(&0u32);
+                if *p < *key {
+                    result.short_quantile.insert(k, *key);
+                }
+            }
+        }
+    }
+
+    pub fn add(&mut self, another: &RequestLatency) {
+        for (op, another_result) in another.op_latency.iter() {
+            if !self.op_latency.contains_key(op) {
+                self.op_latency.insert(op.to_string(), another_result.clone());
+                continue;
+            }
+            let this_result = self.op_latency.get_mut(op.as_str()).unwrap();
+            // 更新bucket和cnt
+            for (t, another_cnt) in another_result.bucket.iter() {
+                let this_cnt = this_result.bucket.get(t).unwrap_or(&0u32);
+                this_result.bucket.insert(*t, *this_cnt + *another_cnt);
+                this_result.op_cnt += another_cnt;
+            }
+        }
+
+        self.update_quantile();
+        // self.cal_clear_res();
+    }
+
+    pub fn cal_qps(&mut self, duration: u64) {
+        for (_op, res) in self.op_latency.iter_mut() {
+            res.qps = res.op_cnt as f32 / duration as f32;
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,402 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time;
+
+use clap::Parser;
+use rand;
+use rand::Rng;
+use serde;
+use serde_json;
+use zookeeper::{Acl, CreateMode, WatchedEvent, Watcher, ZkError, ZooKeeper, ZooKeeperExt};
+
+use zookeeper_benchmark::latency::RequestLatency;
+
+struct LoggingWatcher;
+
+impl Watcher for LoggingWatcher {
+    fn handle(&self, _e: WatchedEvent) {}
+}
+
+const BENCH_ROOT: &str = "/bench_root";
+
+#[derive(Parser, serde::Serialize, Clone)]
+#[command(name = "zookeeper-benchmark")]
+#[command(author = "configcenter")]
+#[command(version = "1.0")]
+#[command(about = "configcenter zookeeper instance benchmark tool", long_about = None)]
+struct Cli {
+    #[arg(long, default_value = "127.0.0.1:2181")]
+    address: String,
+    #[arg(long, default_value_t = 10)]
+    client_num: i32,
+    #[arg(long, default_value_t = 1024)]
+    data_size: i32,
+    #[arg(long, default_value_t = 10000)]
+    node_num: i32,
+    #[arg(long, default_value_t = 100)]
+    duration: u64,
+    #[arg(long, default_value_t = 0.8)]
+    rw_ratio: f32,
+    #[arg(long, default_value = "")]
+    op: String,
+}
+
+
+#[derive(Debug, Clone, serde::Serialize)]
+struct BenchRes {
+    client_num: i32,
+    total_success: i32,
+    total_failure: i32,
+    metrics: RequestLatency,
+    duration: i32,
+    total_qps: f32,
+}
+
+impl BenchRes {
+    fn new(client_num: i32, duration: i32) -> BenchRes {
+        BenchRes {
+            client_num,
+            duration,
+            total_success: 0,
+            total_failure: 0,
+            total_qps: 0.0,
+            metrics: RequestLatency::new(),
+        }
+    }
+}
+
+// 预先创建znode
+fn pre_create(params: &Cli) {
+    let zk = ZooKeeper::connect(params.address.as_str(), time::Duration::from_secs(15), LoggingWatcher).unwrap();
+
+    // 如果是多客户端节点并行压测时,则只需要一个客户端进行预先创建节点
+    // 先删除测试目录
+    _ = zk.delete_recursive(BENCH_ROOT);
+
+    // 创建测试根node
+    let result = zk.create(BENCH_ROOT, vec![0; 10], Acl::open_unsafe().clone(), CreateMode::Persistent);
+    match result {
+        Ok(_) => {}
+        Err(e) => {
+            panic!("create bench root node failed: {}", e)
+        }
+    }
+
+    // 创建子节点
+    for i in 0..params.node_num {
+        let path = format!("{}/{:0>10}", BENCH_ROOT, i);
+        let _result = zk.create(
+            path.as_str(),
+            vec![1; 1024],
+            Acl::open_unsafe().clone(),
+            CreateMode::Persistent,
+        ).unwrap();
+    }
+    _ = zk.close();
+}
+
+// 清理压测数据
+fn post_clean(params: &Cli) {
+    let zk = ZooKeeper::connect(params.address.as_str(), time::Duration::from_secs(15), LoggingWatcher).unwrap();
+
+    // 递归删除节点
+    _ = zk.delete_recursive(BENCH_ROOT);
+}
+
+
+// 压测set操作
+fn bench_set(params: &Cli) -> Option<BenchRes> {
+    let (tx, rx): (mpsc::Sender<BenchRes>, mpsc::Receiver<BenchRes>) = mpsc::channel();
+
+    let mut handles = vec![];
+    for i in 0..params.client_num {
+        let thread_sender = tx.clone();
+        let param = params.clone();
+        let mut random_value: Vec<u8> = vec![0; param.data_size as usize];
+
+        let handle = thread::Builder::new()
+            .name(format!("thread-{:0>4}", i))
+            .spawn(move || {
+                let mut zk = connect_zk(&param.address.as_str());
+
+                let mut rng = rand::thread_rng();
+                // 当前线程的压测结果
+                let mut res = BenchRes::new(param.client_num, param.duration as i32);
+                // 当前线程压测开始时间
+                let start_time = time::Instant::now();
+                loop {
+                    if time::Instant::now().duration_since(start_time).as_secs() > param.duration {
+                        // 计算单个连接的QPS
+                        thread_sender.send(res).unwrap();
+                        break;
+                    }
+
+                    // 随机set某一个node
+                    let idx: i32 = rng.gen_range(0..param.node_num);
+                    let path = format!("{}/{:0>10}", BENCH_ROOT, idx);
+
+                    // 随机改变value某一位的值
+                    let random_index = rng.gen_range(0..param.data_size);
+                    random_value[random_index as usize] = rng.gen_range(0..10) as u8;
+                    let start = time::Instant::now();
+                    let result = zk.set_data(&path, random_value.clone(), Some(-1));
+                    match result {
+                        Ok(_) => {
+                            res.total_success += 1;
+                            res.metrics.insert(param.op.to_string(), time::Instant::now().duration_since(start).as_millis() as u32);
+                        }
+                        Err(e) => {
+                            println!("failed to set znode {}, error: {}. Reconnecting...", path, e);
+                            res.total_failure += 1;
+                            _ = zk.close(); // 先close一下,不管result
+                            match e {
+                                ZkError::ConnectionLoss => {
+                                    zk = connect_zk(param.address.as_str()); // 重连zk
+                                }
+                                _ => {
+                                    // 暂时还是重连zk
+                                    zk = connect_zk(param.address.as_str());
+                                }
+                            }
+                        }
+                    }
+                }
+                _ = zk.close();
+            });
+        handles.push(handle.unwrap())
+    }
+    for handle in handles {
+        _ = handle.join();
+    }
+    drop(tx);
+
+    let mut total_res = BenchRes::new(params.client_num, params.duration as i32);
+    for res in rx.iter() {
+        println!("cnt from tx: {:?}", serde_json::to_string(&res).unwrap());
+        total_res.total_success += res.total_success;
+        total_res.total_failure += res.total_failure;
+        total_res.metrics.add(&res.metrics);
+    }
+    total_res.total_qps = total_res.total_success as f32 / total_res.duration as f32;
+    total_res.metrics.cal_qps(params.duration);
+    Some(total_res)
+}
+
+
+// 压测get操作
+fn bench_get(params: &Cli) -> Option<BenchRes> {
+    let (tx, rx): (mpsc::Sender<BenchRes>, mpsc::Receiver<BenchRes>) = mpsc::channel();
+
+    let mut handles = vec![];
+    for i in 0..params.client_num {
+        let thread_sender = tx.clone();
+        let param = params.clone();
+
+        let handle = thread::Builder::new()
+            .name(format!("thread-{:0>4}", i))
+            .spawn(move || {
+                let mut zk_cli = connect_zk(&param.address.as_str());
+
+                let mut rng = rand::thread_rng();
+                // 当前线程的压测结果
+                let mut res = BenchRes {
+                    client_num: 1,
+                    total_success: 0,
+                    total_failure: 0,
+                    metrics: RequestLatency::new(),
+                    duration: param.duration as i32,
+                    total_qps: 0.0,
+                };
+                // 当前线程压测开始时间
+                let start_time = time::Instant::now();
+                loop {
+                    if time::Instant::now().duration_since(start_time).as_secs() > param.duration {
+                        // 计算单个连接的QPS
+                        thread_sender.send(res).unwrap();
+                        break;
+                    }
+
+                    // 随机get一个znode
+                    let idx: i32 = rng.gen_range(0..param.node_num);
+                    let path = format!("{}/{:0>10}", BENCH_ROOT, idx);
+
+                    let start = time::Instant::now();
+                    let result = zk_cli.get_data(&path, false);
+                    match result {
+                        Ok(_) => {
+                            res.total_success += 1;
+                            res.metrics.insert(param.op.to_string(), time::Instant::now().duration_since(start).as_millis() as u32);
+                        }
+                        Err(e) => {
+                            println!("failed to set znode {}, error: {}. Reconnecting...", path, e);
+                            res.total_failure += 1;
+                            _ = zk_cli.close(); // 先close一下,不管result
+                            match e {
+                                ZkError::ConnectionLoss => {
+                                    zk_cli = connect_zk(param.address.as_str()); // 重连zk
+                                }
+                                _ => {
+                                    // 暂时还是重连zk
+                                    zk_cli = connect_zk(param.address.as_str());
+                                }
+                            }
+                        }
+                    }
+                }
+                _ = zk_cli.close();
+            });
+        handles.push(handle.unwrap())
+    }
+    for handle in handles {
+        _ = handle.join();
+    }
+    drop(tx);
+
+    // 汇总结果
+    let mut total_res = BenchRes::new(params.client_num, params.duration as i32);
+    for res in rx.iter() {
+        // println!("cnt from tx: {:?}", serde_json::to_string(&res).unwrap());
+        total_res.total_success += res.total_success;
+        total_res.total_failure += res.total_failure;
+        total_res.metrics.add(&res.metrics);
+    }
+
+    total_res.total_qps = total_res.total_success as f32 / params.duration as f32;
+    total_res.metrics.cal_qps(params.duration);
+    Some(total_res)
+}
+
+
+// 压测getset操作
+fn bench_getset(params: &Cli) -> Option<BenchRes> {
+    let (tx, rx): (mpsc::Sender<BenchRes>, mpsc::Receiver<BenchRes>) = mpsc::channel();
+
+    let mut handles = vec![];
+    for i in 0..params.client_num {
+        let thread_sender = tx.clone();
+        let param = params.clone();
+        let mut random_value: Vec<u8> = vec![0; param.data_size as usize];
+
+        let handle = thread::Builder::new()
+            .name(format!("thread-{:0>4}", i))
+            .spawn(move || {
+                let mut zk_cli = connect_zk(&param.address.as_str());
+
+                let mut rng = rand::thread_rng();
+                // 当前线程的压测结果
+                let mut res = BenchRes::new(param.client_num, param.duration as i32);
+                // 当前线程压测开始时间
+                let start_time = time::Instant::now();
+                loop {
+                    if time::Instant::now().duration_since(start_time).as_secs() > param.duration {
+                        // 计算单个连接的QPS
+                        thread_sender.send(res).unwrap();
+                        break;
+                    }
+
+                    // 随机get一个znode
+                    let idx: i32 = rng.gen_range(0..param.node_num);
+                    let path = format!("{}/{:0>10}", BENCH_ROOT, idx);
+
+                    let start = time::Instant::now();
+                    if rng.gen_range(0.0..=1.0) < param.rw_ratio {
+                        let get_result = zk_cli.get_data(&path, false);
+                        match get_result {
+                            Ok(_) => {
+                                res.total_success += 1;
+                                res.metrics.insert("get".to_string(), time::Instant::now().duration_since(start).as_millis() as u32);
+                            }
+                            Err(e) => {
+                                println!("failed to set znode {}, error: {}. Reconnecting...", path, e);
+                                res.total_failure += 1;
+                                _ = zk_cli.close(); // 先close一下,不管result
+                                zk_cli = connect_zk(param.address.as_str()); // 重连zk
+                            }
+                        }
+                    } else {
+                        // 随机改变value某一位的值
+                        let random_index = rng.gen_range(0..param.data_size);
+                        random_value[random_index as usize] = rng.gen_range(0..10) as u8;
+
+                        let set_result = zk_cli.set_data(&path, random_value.clone(), Some(-1));
+                        match set_result {
+                            Ok(_) => {
+                                res.total_success += 1;
+                                res.metrics.insert("set".to_string(), time::Instant::now().duration_since(start).as_millis() as u32);
+                            }
+                            Err(e) => {
+                                println!("failed to set znode {}, error: {}. Reconnecting...", path, e);
+                                res.total_failure += 1;
+                                _ = zk_cli.close(); // 先close一下,不管result
+                                zk_cli = connect_zk(param.address.as_str()); // 重连zk
+                            }
+                        }
+                    }
+                }
+                _ = zk_cli.close();
+            });
+        handles.push(handle.unwrap())
+    }
+    for handle in handles {
+        _ = handle.join();
+    }
+    drop(tx);
+
+    // 汇总结果
+    let mut total_res = BenchRes::new(params.client_num, params.duration as i32);
+    for res in rx.iter() {
+        // println!("cnt from tx: {:?}", serde_json::to_string(&res).unwrap());
+        total_res.total_success += res.total_success;
+        total_res.total_failure += res.total_failure;
+        total_res.metrics.add(&res.metrics);
+    }
+
+    total_res.total_qps = total_res.total_success as f32 / params.duration as f32;
+    total_res.metrics.cal_qps(params.duration);
+    Some(total_res)
+}
+
+// 重新连接zk
+fn connect_zk(addr: &str) -> ZooKeeper {
+    loop {
+        match ZooKeeper::connect(addr, time::Duration::from_secs(10), LoggingWatcher) {
+            Ok(zk) => {
+                return zk;
+            }
+            Err(e) => {
+                println!("Error connecting to ZooKeeper: {}", e);
+                thread::sleep(time::Duration::from_millis(5));
+            }
+        }
+    }
+}
+
+fn main() {
+    // 解析命令行参数
+    let params = Cli::parse();
+    println!("benchmark parameters: {}", serde_json::to_string(&params.clone()).unwrap());
+
+    match params.op.as_str() {
+        "pre-create" => {
+            pre_create(&params);
+        }
+        "post-clean" => {
+            post_clean(&params);
+        }
+        "set" => {
+            let bench_res = bench_set(&params);
+            println!("{}", serde_json::to_string(&bench_res).unwrap());
+        }
+        "get" => {
+            let bench_res = bench_get(&params);
+            println!("{}", serde_json::to_string(&bench_res).unwrap());
+        }
+        "getset" => {
+            let bench_res = bench_getset(&params);
+            println!("{}", serde_json::to_string(&bench_res).unwrap());
+        }
+        _ => {
+            panic!("operation {} is not supported yet!", params.op)
+        }
+    };
+}
